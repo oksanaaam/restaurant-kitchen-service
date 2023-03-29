fbd7e0abc1507c59d1d969c800933b26aebf8e9b
name <ksyusha0kubay@gmail.com>
2023-03-29 12:08:29 +0300

    Add models in project

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name}, dish_type: {self.dish_type}, price: {self.price}"
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return (f"{self.username}, email: {self.email}, "
+                f"years of experience: {self.years_of_experience}")
