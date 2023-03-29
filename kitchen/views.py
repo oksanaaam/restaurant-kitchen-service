1f184eb360e05a2a051b544e4dd79024688649e3
name <ksyusha0kubay@gmail.com>
2023-03-29 16:39:12 +0300

    Add registration app with user authentication

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,7 @@
 from urllib.request import Request
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -7,6 +9,7 @@ from django.views import generic
 from kitchen.models import Dish, DishType, Cook
 
 
+@login_required
 def index(request: Request) -> HttpResponse:
     num_dish_types = DishType.objects.count()
     num_dishes = Dish.objects.count()
@@ -25,7 +28,7 @@ def index(request: Request) -> HttpResponse:
     return render(request, "kitchen/index.html", context=context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type_list.html"
     context_object_name = "dish_types_list"
@@ -33,20 +36,20 @@ class DishTypeListView(generic.ListView):
     paginate_by = 5
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     paginate_by = 5
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
 
-class CookDetailView(generic.DetailView):
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
     queryset = Cook.objects.prefetch_related("dishes")
