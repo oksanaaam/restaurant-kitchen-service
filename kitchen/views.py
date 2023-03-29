a1826d575f64c4882edab7f26bf05038ab93c341
name <ksyusha0kubay@gmail.com>
2023-03-29 13:04:53 +0300

    Create urls.py in kitchen and add new views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,20 @@
+from urllib.request import Request
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, DishType, Cook
+
+
+def index(request: Request) -> HttpResponse:
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
