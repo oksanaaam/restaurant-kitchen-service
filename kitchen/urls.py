13ca871a558bd00b86a8323b3be24b34a7e960fa
name <ksyusha0kubay@gmail.com>
2023-03-29 16:04:10 +0300

    Add views to project

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
-from kitchen.views import index
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView
+)
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish-types",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path(
+        "cooks/<int:pk>/",
+        CookDetailView.as_view(),
+        name="cook-detail"
+    ),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -2,6 +2,7 @@ from urllib.request import Request
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import Dish, DishType, Cook
 
@@ -18,3 +19,30 @@ def index(request: Request) -> HttpResponse:
     }
 
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_type_list.html"
+    context_object_name = "dish_types_list"
+    queryset = DishType.objects.order_by("name")
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    paginate_by = 5
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.prefetch_related("dishes")
