8f4bf986a98e100b2be7f2db76966efee2b72c6b
name <ksyusha0kubay@gmail.com>
2023-04-03 09:30:27 +0300

    Change settings INSTALLED_APPS

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap4",
-    "restaurant",
-    "kitchen",
+    "restaurant.kitchen",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
