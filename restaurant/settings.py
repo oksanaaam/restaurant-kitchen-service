ae4d84fc6b7a509123d4e5dc0f17fc9f54e1786b
name <ksyusha0kubay@gmail.com>
2023-04-05 11:42:49 +0300

    Add restaurant-sh19.onrender.com

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://restaurant-sh19.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-sh19.onrender.com"]
 
 
 # Application definition
